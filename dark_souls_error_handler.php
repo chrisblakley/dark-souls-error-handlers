5fd8865fcd145cfc937608c74bc4ab34c63f30a8
Chris Blakley <greatblakes@gmail.com>
2015-01-21 19:30:31 -0500

    Update dark_souls_error_handler.php

--- dark_souls_error_handler.php
+++ dark_souls_error_handler.php
@@ -4,8 +4,7 @@
 	*
 	* Include this file using require_once('dark_souls_error_handler.php'); to begin your journey.
 	*
-	* Thy intent is to persevere... to the bitter end, hmm?
-	* Hah hah hah! Good, good. What is bravery, without a dash of recklessness!
+	* "What is bravery without a dash of recklessness?"
 	*
 	*/
 	
