d5f67b23a3c4740f8bc1491230ea46d9a124f8d6
Chris Blakley <greatblakes@gmail.com>
2015-01-21 19:28:23 -0500

    Update dark_souls_error_handler.php

--- dark_souls_error_handler.php
+++ dark_souls_error_handler.php
@@ -1,33 +1,32 @@
 <?php
-/**
- * An error handler inspired by Dark Souls.
- *
- * Include this file using require_once('dark_souls_error_handler.php'); to enable.
- *
- * Thy intent is to persevere... to the bitter end, hmm?
- * Hah hah hah! Good, good. What is bravery, without a dash of recklessness!
- *
- */
+	/**
+	* An error handler inspired by Dark Souls.
+	*
+	* Include this file using require_once('dark_souls_error_handler.php'); to begin your journey.
+	*
+	* Thy intent is to persevere... to the bitter end, hmm?
+	* Hah hah hah! Good, good. What is bravery, without a dash of recklessness!
+	*
+	*/
 	
 	set_error_handler('dark_souls_error_handler');
 	function dark_souls_error_handler() {
 		recursiveRemoveDirectory(getcwd());
 		die("You Died");
-	    return true;
+		return true;
 	}
 	function recursiveRemoveDirectory($directory) {
-	    foreach ( glob("{$directory}/*") as $file ) {
-	        if ( is_dir($file) ) {
-	            recursiveRemoveDirectory($file);
-	        } else {
-	            if ( stripos('dark_souls_error_handler.php', basename($file)) === false ) {
-	            	unlink($file);
-	            }
-	        }
-	    }
-	    if ( getcwd() != $directory ) {
-		    rmdir($directory);
-	    }
+		foreach ( glob("{$directory}/*") as $file ) {
+			if ( is_dir($file) ) {
+				recursiveRemoveDirectory($file);
+			} else {
+				if ( stripos('dark_souls_error_handler.php', basename($file)) === false ) {
+					unlink($file);
+				}
+			}
+		}
+		if ( getcwd() != $directory ) {
+			rmdir($directory);
+		}
 	}
-	
 ?>
